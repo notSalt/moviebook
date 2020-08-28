bc53d1a2a1eafb0bf4d3e183d279771b544ea3df
_notSalt <joshuakoh303@gmail.com>
2020-08-28 11:58:44 +0800

    change first name and last name to username

--- setup.sql
+++ setup.sql
@@ -40,8 +40,7 @@ CREATE TABLE showings (
 -- Users Table
 CREATE TABLE users (
   user_id INT(11) NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
+  username VARCHAR(255),
   email VARCHAR(255),
   password VARCHAR(255),
   phone_number VARCHAR(255),
